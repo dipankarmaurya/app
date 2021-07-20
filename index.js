450001a0435a0eb835f4010b5705a8bdfa690317
Dipankar Maurya <dipankarmaurya5855@gmail.com>
2021-07-20 18:57:43 +0530

    seting up static files for layouts and pages

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.use(express.static('./assets'));
 
 // we have to use layouts section before the router section
  app.use(expressLayouts);
+
+//extracts styles and javascript from sub pages and  render it to layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
 //setting up ejs engine to use html file
  app.set('view engine','ejs');
  app.set('views','./views');
